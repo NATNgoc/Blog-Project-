6f0fc8f8229725f5bca01b53efd61f8bb3984a2f
NATNgoc <nguyenatn2003@gmail.com>
2023-11-17 10:42:07 +0700

    add apikey service with create, update status, add permission

--- BE/src/models/repository/apikey.repo.js
+++ BE/src/models/repository/apikey.repo.js
@@ -1,5 +1,5 @@
 const ApiKeyModel = require('../apikey.model')
-
+const crypto = require('crypto')
 
 class ApiKeyRepository {
 
@@ -7,6 +7,46 @@ class ApiKeyRepository {
         return await ApiKeyModel.findOne({ ...filter })
             .select({ ...select })
             .lean()
+    }
+
+    static async createNewApiKey(apikey, permission = ['0000'], status = true) {
+        const newObject = {
+            key: apikey,
+            permisson: permission,
+            status: status
+        }
+        return await ApiKeyModel.create({ ...newObject })
+    }
+
+    static async updateApiKey(filter, bodyUpdate, isNew = true) {
+        return await ApiKeyModel.updateOne({ ...filter }, {
+            $set: {
+                ...bodyUpdate
+            }
+        }, {
+            new: isNew
+        })
+    }
+
+
+    static async addPermission(filter, permission) {
+        return await ApiKeyModel.updateOne({ ...filter }, {
+            $addToSet: {
+                permissions: permission
+            }
+        }, {
+            new: isNew
+        })
+    }
+
+    static async removePermission(filter, permission) {
+        return await ApiKeyModel.updateOne({ ...filter }, {
+            $addToSet: {
+                permissions: permission
+            }
+        }, {
+            new: isNew
+        })
     }
 
 }
--- BE/src/services/apikey.service.js
+++ BE/src/services/apikey.service.js
@@ -3,7 +3,7 @@ const ApiKeyRepository = require('../models/repository/apikey.repo')
 
 class ApiKeyService {
 
-    static async checkApiKey(apikey) {
+    static async getActiveApiKey(apikey) {
         if (!apikey) throw new Error.BadRequestError('Unvalid Api Key')
         const filter = {
             "key": apikey,
@@ -12,6 +12,37 @@ class ApiKeyService {
         const currentApiKey = await ApiKeyRepository.findApiKey(filter)
         if (!currentApiKey) throw new Error.BadRequestError('Unvalid Api Key')
         return currentApiKey
+    }
+
+
+    static async createNewApiKey(permission) {
+        const randomKey = await crypto.randomBytes(64).toString()
+        return await ApiKeyRepository.createNewApiKey(randomKey, permission)
+    }
+
+    static async updateApiKeyStatus(key, status) {
+        if (!key || !status) throw new Error.BadRequestError("Check request body!")
+        const filter = {
+            key: key
+        }
+        const updateStatusConfig = {
+            status: status
+        }
+        return await ApiKeyRepository.updateApiKey(filter, updateStatusConfig, true)
+    }
+
+    static async addPermission(key, newPermission) {
+        const currentApiKey = await this.getActiveApiKey(key)
+        currentApiKey.permissions.add(newPermission)
+        await currentApiKey.save()
+    }
+
+    static async removePermission(key, oldPermission) {
+        const currentApiKey = await this.getActiveApiKey(key)
+        currentApiKey.permissions = currentApiKey.permissions.filter(permission => {
+            permission !== oldPermission
+        })
+        await currentApiKey.save()
     }
 
 }
